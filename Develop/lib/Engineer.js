27ccc7bbc10b3930b400f6b8e07eeb7b4b05aaf8
Lauren Hutchinson <LaurenHutchinson@Laurens-MacBook-Pro-7.local>
2020-12-06 19:01:05 -0800

    employee constructor

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+class Engineer {
+
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = "Employee";
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    getEmail(){
+        return this.email;
+    }
+
+    getRole(){
+        return this.role;
+    }
+
+
+}
+
+module.exports = Engineer;
